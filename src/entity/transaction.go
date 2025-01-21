6aae4ea34067bc057d6244b7fe2f5c512f9c63df
adrinant <adrinant2000@gmail.com>
2025-01-21 20:50:44 +0700

    edit transaction entity

--- src/entity/transaction.go
+++ src/entity/transaction.go
@@ -1,10 +1,10 @@
 package entity
 
 type Transaction struct {
-	ID     int     `db:"id" json:"id" binding:"required,gt=0"`
+	ID     int     `db:"id" json:"id" binding:"omitempty,gt=0"`
 	Type   string  `db:"type" json:"type" binding:"required,gte=1,lte=5"`
 	Ticker string  `db:"ticker" json:"ticker" binding:"required,gte=1,lte=30"`
 	Volume float64 `db:"volume" json:"volume" binding:"required"`
 	Price  float64 `db:"price" json:"price" binding:"required"`
-	Date   string  `db:"date" json:"date" binding:"reuired"`
+	Date   string  `db:"date" json:"date" binding:"required"`
 }
